c8f98f47c2ea6f2e5561c6e0be1257beda661ad4
andrewzn <ondrejzeman23@gmail.com>
2024-12-08 21:58:16 +0100

    Add keybinds for neotree, moving between windows, Add descriptions for keybinds

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,31 +1,42 @@
 local keymap = vim.keymap
 
-keymap.set('n', 'x', '"_x')
+-- space as leader key
+vim.g.mapleader = " "
+
+keymap.set('n', 'x', '"_x', { desc = 'Delete character without yanking' })
 
 -- Increment/decrement
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
+keymap.set('n', '+', '<C-a>', { desc = 'Increment number' })
+keymap.set('n', '-', '<C-x>', { desc = 'Decrement number' })
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
+keymap.set('n', 'dw', 'vb"_d', { desc = 'Delete word backwards without yanking' })
 
 -- Select all
-keymap.set('n', '<C-a>', 'gg<S-v>G')
+keymap.set('n', '<C-a>', 'gg<S-v>G', { desc = 'Select all content' })
 
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- New tab
-keymap.set('n', 'te', ':tabedit')
+keymap.set('n', 'te', ':tabedit', { desc = 'Open a new tab' })
+
+-- Window navigation using <Space> + w + h/j/k/l
+vim.keymap.set('n', '<Leader>wh', '<C-w>h', { desc = 'Move to left window' })
+vim.keymap.set('n', '<Leader>wj', '<C-w>j', { desc = 'Move to window below' })
+vim.keymap.set('n', '<Leader>wk', '<C-w>k', { desc = 'Move to window above' })
+vim.keymap.set('n', '<Leader>wl', '<C-w>l', { desc = 'Move to right window' })
+
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+keymap.set('n', 'ss', ':split<Return><C-w>w', { desc = 'Split window horizontally' })
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { desc = 'Split window vertically' })
+
 -- Move window
-keymap.set('n', '<Space>', '<C-w>w')
-keymap.set('', 'sh', '<C-w>h')
-keymap.set('', 'sk', '<C-w>k')
-keymap.set('', 'sj', '<C-w>j')
-keymap.set('', 'sl', '<C-w>l')
+keymap.set('n', '<Space>', '<C-w>w', { desc = 'Cycle through windows' })
+keymap.set('', 'sh', '<C-w>h', { desc = 'Move to left window' })
+keymap.set('', 'sk', '<C-w>k', { desc = 'Move to window above' })
+keymap.set('', 'sj', '<C-w>j', { desc = 'Move to window below' })
+keymap.set('', 'sl', '<C-w>l', { desc = 'Move to right window' })
 
 -- Resize window
 keymap.set('n', '<C-w><left>', '<C-w><')
@@ -36,4 +47,5 @@ keymap.set('n', '<C-w><down>', '<C-w>-')
 -- Grammar check
 keymap.set('n', '<F6>', ':setlocal spell! spelllang=en_us<CR>')
 
-vim.g.mapleader = " "
+-- Toggle Neo-tree
+keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = 'Toggle Neo-tree' })
