9f5ef4b343151cd6c005bb16831562543523d661
andrewzn <ondrejzeman23@gmail.com>
2024-12-17 12:27:49 +0100

    Add option to open working dir in neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -4,6 +4,7 @@ return {
 	dependencies = { "MunifTanjim/nui.nvim", lazy = true },
 	opts = function(_, opts)
 		opts.filesystem = {
+			follow_current_file = { enabled = true },
 			filtered_items = {
 				visible = true,
 				hide_dotfiles = false,
